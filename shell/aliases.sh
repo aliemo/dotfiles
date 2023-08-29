3e759a158ed7cbe004b1849e42980ee393752c71
Ali Mohammadpour <aliemohammadpour@gmail.com>
2023-08-30 01:29:06 +0330

    Add Aliases for virtualenv and source

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -9,6 +9,37 @@ alias xxtools="cd $QTOOLS_PATH"
 alias xxtemp="cd /tmp/"
 ## }}}
 
+alias xrcz=". ~/.zshrc"
+alias xrcbash=". ~/.bashrc"
+
+## Python Env {{{
+function xvenv () {
+
+        if [ $1 = "c" ]; then 
+                virtualenv .venv
+
+        elif [ $1 = "s" ]; then
+                . .venv/bin/activate
+        
+        elif [ $1 = "r" ]; then
+                rm -rf .venv
+
+        
+        elif [ $1 = "cn" ]; then
+                virtualenv $2
+
+        elif [ $1 = "sn" ]; then
+                . $2/bin/activate
+        
+        elif [ $1 = "rn" ]; then
+                rm -rf $2
+
+        elif [ $1 = "d" ]; then
+                deactivate
+        fi 
+}
+#}}}
+
 ## Emacs {{{
 # aliases
 alias emacs='emacs -nw'
@@ -20,7 +51,7 @@ alias xemacs='emacs-gtk'
 XILTOOLS_VERSION='2023.1'
 alias envvivado='source $XIL_TOOLS_PATH/Vivado/$XILTOOLS_VERSION/.settings64-Vivado.sh'
 alias envvitis='source $XIL_TOOLS_PATH/Vitis/$XILTOOLS_VERSION/.settings64-Vitis.sh'
-alias envvhls='source $XIL_TOOLS_PATH/Vitis_HLS/$XILTOOLS_VERSION/.settings64-Vitis.sh'
+alias envvhls='source $XIL_TOOLS_PATH/Vitis_HLS/$XILTOOLS_VERSION/.settings64-Vitis_HLS.sh'
 alias envdocnav='source $XIL_TOOLS_PATH/DocNav/.settings64-DocNav.sh'
 alias envmcomposer='source $XIL_TOOLS_PATH/Model_Composer/$XILTOOLS_VERSION/.settings64-Model_Composer.sh'
 alias envptlnx='source $XIL_TOOLS_PATH/petalinux/$XILTOOLS_VERSION/settings.sh'
